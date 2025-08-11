3f9c7294b508ab72c8590257038330a735e2bcdd
Satya Sankalp Gowravajhal <86484291+SatyaSankalp0402@users.noreply.github.com>
2025-08-11 15:27:03 +0200

    Create Pair_Number_Generator.py

---
+++ Pair_Number_Generator.py
@@ -0,0 +1,10 @@
+def get_pair_number_from_color(major_color, minor_color):
+  try:
+    major_index = MAJOR_COLORS.index(major_color)
+  except ValueError:
+    raise Exception('Major index out of range')
+  try:
+    minor_index = MINOR_COLORS.index(minor_color)
+  except ValueError:
+    raise Exception('Minor index out of range')
+  return major_index * len(MINOR_COLORS) + minor_index + 1
