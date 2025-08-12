7d82e96a94ef5588358a81cd7c99fab138000a6d
Satya Sankalp Gowravajhal <86484291+SatyaSankalp0402@users.noreply.github.com>
2025-08-12 06:04:37 +0200

    Update Wiring_Manual_Generation.py

--- Wiring_Manual_Generation.py
+++ Wiring_Manual_Generation.py
@@ -9,5 +9,5 @@ def color_coding_manual(major_colors,minor_colors):
   for major_color in major_colors:
     for minor_color in minor_colors:
       color_combination = color_pair_to_string(major_color,minor_color)
-      print_color_coding_manual(color_combination)
+      print("{:<20} | {:<10}".format(color_combination, pair_number))
       pair_number +=1
